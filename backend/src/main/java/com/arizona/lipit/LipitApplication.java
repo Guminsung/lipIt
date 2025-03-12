3d5069f2d5399711683dca33770b9894428a2d94
서미지 <itsanisland@naver.com>
2025-03-12 15:43:43 +0900

    chore: 프로젝트 초기 설정 및 환경 구성
    - 프로젝트 의존성 추가
    - Swagger API 문서 연동
    
    S12P21D102-16 #done

---
+++ backend/src/main/java/com/arizona/lipit/LipitApplication.java
@@ -0,0 +1,14 @@
+package com.arizona.lipit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class LipitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LipitApplication.class, args);
+    }
+
+}
