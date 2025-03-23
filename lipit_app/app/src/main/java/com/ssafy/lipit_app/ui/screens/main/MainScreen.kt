1f6a7e7eff07dde81d5951dd2959c7fe0b39c6dd
서정후 <jeonghu1010@naver.com>
2025-03-23 23:59:39 +0900

    feat:메인페이지 오늘의 문장 UI 구현

--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/main/MainScreen.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/main/MainScreen.kt
@@ -2,10 +2,13 @@ package com.ssafy.lipit_app.ui.screens.main
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -14,7 +17,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -36,7 +41,7 @@ fun MainScreen(
             .padding(horizontal = 20.dp, vertical = 60.dp)
     ){
         UserInfoSection(state.userName) // 상단의 유저 이름, 등급 부분
-//        TodaysSentence() // 오늘의 문장
+        TodaysSentence() // 오늘의 문장
 //        WeeklyCallsSection(
 //            selectedDay = state.selectedDay,
 //            callItems = state.items,
@@ -50,6 +55,7 @@ fun MainScreen(
 }
 
 
+// 사용자 정보 (이름 & 등급)
 @Composable
 fun UserInfoSection(userName: String) {
     Row(
@@ -62,7 +68,7 @@ fun UserInfoSection(userName: String) {
                 fontSize = 20.sp,
                 lineHeight = 50.sp,
                 fontFamily = FontFamily(Font(R.font.sf_pro)),
-                fontWeight = FontWeight(510),
+                fontWeight = FontWeight.Medium,
                 color = Color(0xFF000000),
             )
         )
@@ -77,6 +83,75 @@ fun UserInfoSection(userName: String) {
         )
     }
 }
+
+// 오늘의 문장
+@Composable
+fun TodaysSentence(){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 9.dp),
+    ) {
+        // 배경
+        Image(
+            painter = painterResource(id = R.drawable.main_todays_sentence_background),
+            contentDescription = "오늘의 명언 배경",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(110.dp)
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(110.dp)
+            ) {
+                // 오늘의 문장 원문 + 번역 텍스트
+            Column(
+                modifier = Modifier.weight(1f)
+                    .align(Alignment.CenterVertically)
+                    .padding(top = 21.dp, start = 27.dp, bottom = 21.dp, end = 6.dp)
+                ) {
+                Text(
+                    text = "With your talent and hard work, sky’s the limit!",
+                    style = TextStyle(
+                        fontSize = 15.sp,
+                        lineHeight = 20.sp,
+                        fontFamily = FontFamily(Font(R.font.sf_pro)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFFFFFFFF),
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(7.dp))
+
+                Text(
+                    text = "너의 재능과 노력이라면, 한계란 없지!  ✦˚",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        lineHeight = 20.sp,
+                        fontFamily = FontFamily(Font(R.font.sf_pro)),
+                        fontWeight = FontWeight(700),
+                        color = Color(0xFFFFFFFF),
+                    )
+                )
+            }
+
+            Image(
+                painterResource(id = R.drawable.main_todays_sentance_img),
+                contentDescription = "오늘의 문장 이미지",
+                modifier = Modifier
+                    .height(120.dp)
+                    .width(80.dp)
+                    .align(Alignment.Top)
+                    .padding(start = 0.dp, top = 12.dp, bottom = 20.dp, end = 15.dp)
+            )
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
