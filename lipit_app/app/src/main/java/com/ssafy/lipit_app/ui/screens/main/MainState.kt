82e7e568c6d9263fc1b54af46f5b8c680461922c
서정후 <jeonghu1010@naver.com>
2025-03-23 16:40:58 +0900

    feat: 메인페이지 UserInfoSection UI 구현

--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/main/MainState.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/main/MainState.kt
@@ -1,4 +1,21 @@
 package com.ssafy.lipit_app.ui.screens.main
 
-class MainState {
-}
+// state: 화면에 보여줄 모든 데이터 상태가 담김
+
+data class MainState(
+    val userName: String = "Sarah",
+    val isLoading: Boolean = false,
+    val selectedDay: String = "월",
+    val items: List<CallItem> = emptyList(), // 해당 요일의 통화 리스트
+    val sentenceProgress: Int = 90,
+    val wordProgress: Int = 50,
+    val attendanceCount: Int = 20,
+    val attendanceTotal: Int = 20
+)
+
+data class CallItem(
+    val id: Int,
+    val name: String,
+    val topic: String,
+    val time: String
+)
