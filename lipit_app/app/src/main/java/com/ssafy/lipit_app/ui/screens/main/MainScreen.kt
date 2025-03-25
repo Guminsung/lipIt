b15030e25fec94c4b942c25341674360a3f46774
서정후 <jeonghu1010@naver.com>
2025-03-25 10:29:09 +0900

    feat: 메인페이지 Level Up 파트 UI 구현
    Former-commit-id: cf4aedf49d20f507df128777151f4c4155f83081

--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/main/MainScreen.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/main/MainScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -30,7 +31,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -55,7 +61,7 @@ fun MainScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFFDF8FF))
-            .padding(horizontal = 20.dp, vertical = 60.dp)
+            .padding(start = 20.dp, end = 20.dp, top = 40.dp)
     ) {
         UserInfoSection(state.userName) // 상단의 유저 이름, 등급 부분
         TodaysSentence(state.sentenceOriginal, state.sentenceTranslated) // 오늘의 문장
@@ -68,11 +74,262 @@ fun MainScreen(
                 }
             }
         )
-        //todo: 레벨업, Call Log 버튼, 전화 걸기 버튼 부분 추가
 
+        // 레벨업 파트
+        LevelUpBoard(state.sentenceCnt, state.wordCnt, state.attendanceCnt, state.attendanceTotal)
+
+        // todo:  Call Log 버튼
+        CollLog()
+
+        // todo: 전화 걸기 버튼 부분 추가
+        CallButton()
 
     }
 
+}
+
+// 전화 걸기 버튼
+@Composable
+fun CallButton() {
+    Button(onClick = { /*TODO*/ }) {
+        
+    }
+}
+
+// Call log 버튼 (reports & my voices 버튼들 영역)
+@Composable
+fun CollLog() {
+    // 타이틀
+    Text(
+        text = "Call Log+",
+        style = TextStyle(
+            fontSize = 23.sp,
+            lineHeight = 50.sp,
+            fontWeight = FontWeight(700),
+            color = Color(0xFF000000)
+        ),
+        modifier = Modifier
+            .padding(top = 20.dp)
+    )
+    
+    //버튼1 - report
+    Button(onClick = {
+    /*TODO*/
+    }) {
+
+    }
+
+    //버튼2 - My Voices
+    Button(onClick = {
+        /*TODO*/
+    }) {
+
+    }
+}
+
+// 레벨업 보드 파트
+@Composable
+fun LevelUpBoard(sentenceCnt: Int, wordCnt: Int, attendanceCnt: Int, attendanceTotal: Int) {
+    // 임시 규칙1 : 달성률 50% 이상이면 보라색 원, 50% 아래면 회색 원
+    // 임시 규칙2 : 2레벨로 가기 위해 필요한 조건
+    // - 필요 문장 : 100개
+    // - 필요 단어 : 200개
+    // => 생성 가능한 셀럽 보이스 개수에 따라 규칙 정하기
+
+    val sentencePercent = (sentenceCnt*100 / 100).coerceAtMost(100)
+    val wordPercent = (wordCnt * 100 / 200).coerceAtMost(100)
+    val attendancePercent = if(attendanceTotal > 0) (attendanceCnt * 100 / attendanceTotal) else 0
+
+    // 퍼센트 달성률에 따른 컬러 설정
+    val sentenceColor = if(sentencePercent >= 50) Color(0xFFD09FE6) else Color(0xFF6D6D6F)
+    val wordColor = if (wordPercent >= 50) Color(0xFFD09FE6) else Color(0xFF6D6D6F)
+    val attendanceColor = if (attendancePercent == 100) Color(0xFFD09FE6) else Color(0xFF6D6D6F)
+
+    Box(
+        modifier = Modifier
+            .padding(top = 23.dp, bottom = 15.dp)
+    ) {
+        // 제목
+        Text(
+            text = "Level Up",
+            style = TextStyle(
+                fontSize = 23.sp,
+                lineHeight = 45.sp,
+                fontWeight = FontWeight(700),
+                color = Color(0xFF000000)
+            )
+        )
+
+    }
+
+    // 내용
+    Box(
+        Modifier
+            // 커스텀 그림자 생성
+            .graphicsLayer {
+                shadowElevation = 0f
+                shape = RoundedCornerShape(15.dp)
+                clip = false
+            }
+            .drawBehind {
+                drawIntoCanvas { canvas ->
+                    val paint = Paint().apply {
+                        color = Color(0x14000000)
+                        asFrameworkPaint().maskFilter =
+                            android.graphics.BlurMaskFilter(
+                                30f,
+                                android.graphics.BlurMaskFilter.Blur.OUTER
+                            )
+                    }
+
+                    //그림자 살짝 위로 이동시킴
+                    canvas.drawRoundRect(
+                        left = 0f,
+                        top = 15f,
+                        right = size.width,
+                        bottom = size.height,
+                        radiusX = 30f,
+                        radiusY = 30f,
+                        paint = paint
+                    )
+                }
+            }
+            .fillMaxWidth()
+            .height(150.dp)
+            .background(
+                color = Color(0xFFFFFFFF),
+                shape = RoundedCornerShape(size = 15.dp)
+            )
+            .padding(vertical = 17.dp, horizontal = 20.dp)
+    ){
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ){
+                // 필요 문항 수
+                BadgeWithText(
+                    circleText = "$sentencePercent %",
+                    label = "필요 문장 수",
+                    value = "${100 - sentenceCnt}개",
+                    color = sentenceColor
+                )
+
+                Spacer(modifier = Modifier.width(25.dp))
+
+                // 필요 단어 수
+                BadgeWithText(
+                    circleText = "$wordPercent %",
+                    label = "필요 단어 수",
+                    value = "${200 - wordCnt}",
+                    color = wordColor
+                )
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // 출석률
+            BadgeWithText(
+                circleText = if(attendancePercent == 100) "MAX" else "$attendancePercent %",
+                label = "출석률",
+                value = "$attendanceCnt / $attendanceTotal",
+                color = attendanceColor
+            )
+        }
+
+    }
+
+}
+
+// Level Up에서 각 배지 구성
+@Composable
+fun BadgeWithText(
+    circleText: String,
+    label: String,
+    value: String,
+    color: Color) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 8.dp)
+    ){
+        // 동그라미 뱃지 모양
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .border(2.dp, color, CircleShape)
+                .graphicsLayer {
+                    shadowElevation = 0f
+                    shape = CircleShape
+                    clip = false
+                }
+                .drawBehind {
+                    drawIntoCanvas { canvas ->
+                        val paint = Paint().apply {
+                            this.color = color.copy(alpha = 0.4f)
+                            asFrameworkPaint().maskFilter =
+                                android.graphics.BlurMaskFilter(
+                                    30f,
+                                    android.graphics.BlurMaskFilter.Blur.OUTER
+                                )
+                        }
+
+                        canvas.drawCircle(
+                            center = Offset(size.width / 2, size.height / 2),
+                            radius = size.minDimension / 2,
+                            paint = paint,
+                        )
+                    }
+                },
+            contentAlignment = Alignment.Center
+        ){
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(Color.White, shape = CircleShape),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = circleText,
+                    color = color,
+                    fontSize = 14.sp,
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        lineHeight = 15.sp,
+                        fontWeight = FontWeight(700),
+                        textAlign = TextAlign.Center
+                    )
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Column{
+            Text(
+                text = label,
+                fontSize = 12.sp,
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    lineHeight = 15.sp,
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF5F5F61)
+                )
+            )
+            Text(
+                text = value,
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    lineHeight = 15.sp,
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFF4A4A4C)
+                    )
+            )
+        }
+    }
 }
 
 
@@ -304,7 +561,6 @@ fun dailyCallSchedule(callItems: List<CallItem>) {
                         lineHeight = 15.sp,
                         fontWeight = FontWeight(400),
                         color = Color(0xFF5F5F61),
-
                         )
                 )
             }
@@ -399,9 +655,9 @@ fun MainScreenPreview() {
             callItems = listOf(
                 CallItem(id = 1, name = "Harry Potter", topic = "자유주제", time = "08:00", imageUrl = "https://file.notion.so/f/f/87d6e907-21b3-47d8-98dc-55005c285cce/7a38e4c0-9789-42d0-b8a0-2e3d8c421433/image.png?table=block&id=1c0fd4f4-17d0-80ed-9fa9-caa1056dc3f9&spaceId=87d6e907-21b3-47d8-98dc-55005c285cce&expirationTimestamp=1742824800000&signature=3tw9F7cAaX__HcAYxwEFal6KBsvDg2Gt0kd7VnZ4LcY&downloadName=image.png", "월")
             ),
-            sentenceProgress = 90,
-            wordProgress = 50,
-            attendanceCount = 20,
+            sentenceCnt = 50,
+            wordCnt = 10,
+            attendanceCnt = 20,
             attendanceTotal = 20,
             sentenceOriginal = "With your talent and hard work, sky’s the limit!",
             sentenceTranslated = "너의 재능과 노력이라면, 한계란 없지!",
