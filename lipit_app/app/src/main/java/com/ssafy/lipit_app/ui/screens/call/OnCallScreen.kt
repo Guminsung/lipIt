d87daa825e4d4fb77591c164623d46357a5cfe07
서정후 <jeonghu1010@naver.com>
2025-03-25 17:59:09 +0900

    feat: Call - 통화 정보 & 하단 버튼들 UI 구현
    Former-commit-id: 170567852736dac3bb39ca6a011440343912e50d

--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/call/OnCallScreen.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/call/OnCallScreen.kt
@@ -2,8 +2,10 @@ package com.ssafy.lipit_app.ui.screens.call
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,12 +13,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -35,8 +39,9 @@ fun OnCallScreen(
 ) {
     Box(
         modifier = Modifier
-            .fillMaxSize()
-    ){
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
         // 배경
         Image(
             painterResource(
@@ -48,34 +53,188 @@ fun OnCallScreen(
                 .fillMaxSize()
         )
 
+        // 화면 구성 요소
+        Box(
+            modifier = Modifier
+                .padding(top = 55.dp)
+                .fillMaxSize(),
 
-        // 텍스트 - 보이스 모드 전환 버튼
-        modeChangeBtn(state.CurrentMode)
+            ) {
+            // 텍스트 - 보이스 모드 전환 버튼
+            ModeChangeBtn(state.CurrentMode)
 
-        // 보이스 이름
-        // 남은 통화 시간
-        
-        // 1. 보이스 버전일 경우
-        // 1-1. 자막 ver
-        // 1-1-1. 번역 ver
-        // 1-1-2. no 번역 ver
+            // 통화 정보 (상대방 정보)
+            InfoOfCall(state.voiceName, state.leftTime)
 
-        // 1-2. no 자막 ver
-        // 1-2-1. 자막 없을 경우 번역 버튼 막기
+            // 통화 내용
+            // 1. 보이스 버전일 경우
+            VoiceVersionCall()
 
-        // 2. 텍스트 버전일 경우 -> 디자인 마무리 후 추가 예정
+            // todo: 2. 텍스트 버전일 경우 -> 디자인 마무리 후 추가 예정
+
+            // 하단 버튼들 (메뉴 / 통화 끊기 / 음성 보내기)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                CallActionButtons()
+            }
+        }
+
 
     }
 }
 
+// 하단 버튼 모음
 @Composable
-fun modeChangeBtn(currentMode: String) {
+fun CallActionButtons() {
+    Row(
+        modifier = Modifier
+            .padding(bottom = 60.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround, // 양쪽 끝 정렬
+    ) {
+        // 메뉴
+        Box(
+            modifier = Modifier
+                .width(70.dp)
+                .height(70.dp)
+                .clip(CircleShape)
+                .background(color = Color(0x1AFDF8FF))
+                .clickable {
+                    // todo: 자막 버튼 & 번역 버튼 출력
+
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painterResource(id = R.drawable.oncall_menu_icon),
+                contentDescription = "메뉴",
+                Modifier
+                    .width(39.dp)
+                    .height(62.dp),
+                tint = Color(0xFFFDF8FF)
+            )
+        }
+
+        // 통화 끊기
+        Box(
+            modifier = Modifier
+                .width(80.dp)
+                .height(80.dp)
+                .clip(CircleShape)
+                .background(color = Color(0xFFFE3B31))
+                .clickable {
+                    // todo: 전화 끊기
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painterResource(id = R.drawable.oncall_hangup_icon),
+                contentDescription = "전화 끊기 아이콘",
+                Modifier
+                    .width(70.dp)
+                    .height(80.dp),
+                tint = Color(0xFFFDF8FF)
+            )
+        }
+
+        // 음성 보내기
+        Box(
+            modifier = Modifier
+                .width(70.dp)
+                .height(70.dp)
+                .clip(CircleShape)
+                .background(color = Color(0x1AFDF8FF))
+                .clickable {
+                    // todo: 자막 버튼 & 번역 버튼 출력
+
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painterResource(id = R.drawable.oncall_voice_send_icon),
+                contentDescription = "메뉴",
+                Modifier
+                    .width(39.dp)
+                    .height(62.dp),
+                tint = Color(0xFFFDF8FF)
+            )
+        }
+    }
+}
+
+// Voice 버전 전화
+@Composable
+fun VoiceVersionCall() {
+    // 1-1. 자막 ver
+    CallWithSubtitle()
+
+    // 1-2. no 자막 ver
+    CallWithoutSubtitle()
+}
+
+// Voice 버전 - 자막 X
+@Composable
+fun CallWithoutSubtitle() {
+    // 1-2-1. 자막 없을 경우 번역 버튼 막기
+}
+
+// Voice 버전 - 자막 O
+@Composable
+fun CallWithSubtitle() {
+    // 1-1-1. 번역 ver
+    // 1-1-2. no 번역 ver
+}
+
+// 대화하는 AI 정보 (이름, 남은 시간)
+@Composable
+fun InfoOfCall(voiceName: String, leftTime: String) {
+    Column(
+        modifier = Modifier
+            .padding(top = 60.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // 보이스 이름
+        Text(
+            text = voiceName,
+            style = TextStyle(
+                fontSize = 40.sp,
+                lineHeight = 15.sp,
+                fontWeight = FontWeight(700),
+                color = Color(0xFFFDF8FF),
+                textAlign = TextAlign.Center,
+            )
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // 남은 통화 시간
+        Text(
+            text = leftTime,
+            style = TextStyle(
+                fontSize = 20.sp,
+                lineHeight = 15.sp,
+                fontWeight = FontWeight(400),
+                //fontFamily = FontFamily(Font(R.font.sf_pro)),
+                color = Color(0xFFFDF8FF),
+                textAlign = TextAlign.Center,
+            )
+        )
+    }
+}
+
+@Composable
+fun ModeChangeBtn(currentMode: String) {
     Box(
         modifier = Modifier
-            .padding(top = 75.dp, end = 23.dp)
+            .padding(end = 23.dp)
             .fillMaxWidth(),
         contentAlignment = Alignment.CenterEnd
-    ){
+    ) {
         Row(
             modifier = Modifier
                 .width(81.dp)
@@ -90,7 +249,7 @@ fun modeChangeBtn(currentMode: String) {
         ) {
             // 아이콘
             Icon(
-                painterResource(id = R.drawable.oncall_mode_switch_icon) ,
+                painterResource(id = R.drawable.oncall_mode_switch_icon),
                 contentDescription = "모드 전환 아이콘",
                 tint = Color(0xFFF3E7F9),
             )
@@ -101,7 +260,7 @@ fun modeChangeBtn(currentMode: String) {
             Text(
                 // Voice 모드 -> 버튼 텍스트 Text 출력 (버튼에는 변경될 모드를 출력)
                 // Text 모드 -> 버튼 텍스트 Voice 출력
-                text = if(currentMode == "Text") "Voice" else "Text",
+                text = if (currentMode == "Text") "Voice" else "Text",
                 style = TextStyle(
                     fontSize = 15.sp,
                     lineHeight = 20.sp,
@@ -113,7 +272,6 @@ fun modeChangeBtn(currentMode: String) {
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
