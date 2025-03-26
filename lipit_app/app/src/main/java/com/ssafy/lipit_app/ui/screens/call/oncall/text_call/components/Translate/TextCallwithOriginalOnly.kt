ddd360cfd6fb98f22debe834c99166d50c29fb41
서정후 <jeonghu1010@naver.com>
2025-03-26 17:41:52 +0900

    feat: TextCall - 채팅 화면(번역 포함 버전) UI 구현 완료
    Former-commit-id: 7ef3ed0013240ed60971cbe5222245a9a2035ca2

---
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/call/oncall/text_call/components/Translate/TextCallwithOriginalOnly.kt
@@ -0,0 +1,6 @@
+package com.ssafy.lipit_app.ui.screens.call.oncall.text_call.components.Translate
+
+import com.ssafy.lipit_app.ui.screens.call.oncall.text_call.TextCallState
+
+class TextCallwithOriginalOnly(state: TextCallState) {
+}
