69c7af270a65988b10a57adb51b5fe5bacaffa95
이사랑 <ipcp365@naver.com>
2025-03-27 07:35:06 +0900

    feat: 로그인, 회원가입 화면 생성
    Screen 단위 구성, 간격 및 크기 조절 필요
    
    
    Former-commit-id: a009c4152d69060b67787aea8eaead2b22a0d6c7

---
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/login/LoginScreen.kt
@@ -0,0 +1,162 @@
+package com.ssafy.lipit_app.ui.screens.login
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ssafy.lipit_app.R
+import androidx.compose.runtime.*
+
+
+@Composable
+fun LoginScreen(){
+    val context = LocalContext.current
+
+    // ID
+    var id by remember { mutableStateOf("") }
+
+    // PW
+    var password by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White) // 배경은 나중에 이미지로
+    ) {
+        // 배경 이미지
+        Image(
+            painter = painterResource(id = R.drawable.bg_myvoice),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp),
+            verticalArrangement = Arrangement.SpaceBetween, // 위–아래 간격 자동 분배
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(60.dp)) // 상단 여백
+            // 타이틀 이미지
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "타이틀 로고",
+                modifier = Modifier.size(200.dp)
+            )
+
+            // 하단
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // 왼쪽 정렬 텍스트
+
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                // Input: ID
+                OutlinedTextField(
+                    value = id,
+                    onValueChange = { id = it },
+                    placeholder = { Text("ID", color = Color(0xFFE2C7FF)) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFFE2C7FF),
+                        unfocusedBorderColor = Color(0xFFE2C7FF),
+                        textColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(12.dp),
+                    singleLine = true
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                // Input: PW
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    placeholder = { Text("PW", color = Color(0xFFE2C7FF)) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFFE2C7FF),
+                        unfocusedBorderColor = Color(0xFFE2C7FF),
+                        textColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(12.dp),
+                    singleLine = true,
+                    visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    trailingIcon = {
+                        val icon = if (isPasswordVisible)
+                            painterResource(id = R.drawable.ic_visibility_off)
+                        else
+                            painterResource(id = R.drawable.ic_visibility)
+
+                        IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                            Icon(painter = icon, contentDescription = null, tint = Color(0xFFE2C7FF))
+                        }
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                // 가운데 정렬 버튼 + 텍스트
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CustomFilledButton(text = "LOGIN") {
+                        // 시작 액션
+                    }
+
+                    Spacer(modifier = Modifier.height(12.dp))
+
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview(){
+    LoginScreen()
+}
