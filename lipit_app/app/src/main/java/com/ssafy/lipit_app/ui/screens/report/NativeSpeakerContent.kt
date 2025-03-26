d8d094bd997c0739d2826971eac7c2c02d357947
gaga9353 <gaga9353@naver.com>
2025-03-27 02:32:43 +0900

    feat: 리포트 상세 화면 구현 - 탭뷰 조절 필요 - DTO 형식에 맞추기 S12P21D102-184

---
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/report/NativeSpeakerContent.kt
@@ -0,0 +1,137 @@
+package com.ssafy.lipit_app.ui.screens.report
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NativeSpeakerContent() {
+
+    // 카드 앞면 (뒤집혔을 때 숨김)
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            val dummyItems = List(3) { index -> index }
+            items(dummyItems) { _ ->
+                NativeContent()
+            }
+        }
+
+    }
+
+}
+
+
+@Composable
+fun NativeContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                BorderStroke(1.dp, color = Color.White),
+                shape = RoundedCornerShape(25.dp)
+            )
+            .background(Color.Transparent, shape = RoundedCornerShape(25.dp))
+            .padding(horizontal = 30.dp, vertical = 23.dp)
+    ) {
+        // 제목과 번역
+
+        Text(
+            text = "incorporate AI features",
+            color = Color.White,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 24.sp
+        )
+
+        Text(
+            "AI 기능을 포함하다.",
+            color = Color.White,
+            fontWeight = FontWeight.Light,
+            fontSize = 15.sp,
+            lineHeight = 24.sp
+        )
+
+        Spacer(modifier = Modifier.height(14.dp))
+
+        HorizontalDivider(
+            thickness = 2.dp, color = Color.White.copy(0.5f),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(14.dp))
+
+
+        // 대화 내용 요약
+
+        Column {
+            Text(
+                "나의 문장",
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                "I want to include AI features like hearing. I want to include AI features like hearing.",
+                color = Color.White,
+                fontSize = 14.sp,
+                lineHeight = 24.sp
+            )
+        }
+
+        Spacer(modifier = Modifier.height(14.dp))
+
+
+        // AI 피드백 요약
+
+        Column {
+            Text(
+                "AI 추천 문장",
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                "I want to incorporate AI features such as auditory capabilities.",
+                color = Color.White,
+                fontSize = 14.sp,
+                lineHeight = 24.sp
+            )
+        }
+
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun NativeContentPreview() {
+    NativeContent()
+}
