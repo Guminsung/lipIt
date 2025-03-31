0c80b1eb00affbdc5146f458764d3b971526fdeb
서정후 <jeonghu1010@naver.com>
2025-03-31 15:23:02 +0900

    refactor: 회원가입 screen 코드 screen & intent & viewModel로 분리

--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/auth/Signup/SignupIntent.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/auth/Signup/SignupIntent.kt
@@ -1,4 +1,16 @@
 package com.ssafy.lipit_app.ui.screens.auth.Signup
 
-class SignupIntent {
+//  Intent → ViewModel → State 업데이트
+
+sealed class SignupIntent {
+    data class OnIdChanged(val id: String) : SignupIntent()
+    data class OnPwChanged(val pw: String) : SignupIntent()
+    data class OnPwConfirmChanged(val pwConfirm: String) : SignupIntent()
+    data class OnEnglishNameChanged(val englishName: String) : SignupIntent()
+    data class OnSelectedGenderChanged(val selectedGender: String) : SignupIntent()
+
+    data class OnisPasswordVisible1Changed(val isPasswordVisible_1: Boolean): SignupIntent()
+    data class OnisPasswordVisible2Changed(val isPasswordVisible_2: Boolean): SignupIntent()
+
+    data class OnExpandedChanged(val expanded: Boolean) : SignupIntent()
 }
--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/auth/Signup/SignupViewModel.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/auth/Signup/SignupViewModel.kt
@@ -4,12 +4,56 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class SignupViewModel : ViewModel(){
+class SignupViewModel : ViewModel() {
     private val _state = MutableStateFlow(SignupState())
     val state: StateFlow<SignupState> = _state
 
-    fun onIntent(intent: SignupIntent){
-        when(intent){
+    fun onIntent(intent: SignupIntent) {
+        when (intent) {
+            // id 변경되었을 때
+            is SignupIntent.OnIdChanged -> {
+                // copy를 써서 불변성 유지 + compose recompositon 유도
+                _state.value = _state.value.copy(id = intent.id)
+            }
+
+            // pw 변경되었을 때
+            is SignupIntent.OnPwChanged -> {
+                _state.value = _state.value.copy(pw = intent.pw)
+            }
+
+            // 재입력 pw 변경되었을 때
+            is SignupIntent.OnPwConfirmChanged -> {
+                _state.value = _state.value.copy(pwConfirm = intent.pwConfirm)
+            }
+
+            // 영어 이름 변경되었을 때
+            is SignupIntent.OnEnglishNameChanged -> {
+                _state.value = _state.value.copy(englishName = intent.englishName)
+            }
+
+            // gender 변경되었을 때
+            is SignupIntent.OnSelectedGenderChanged -> {
+                _state.value = _state.value.copy(selectedGender = intent.selectedGender)
+            }
+
+            // ======================================
+
+            // 비밀번호 visible 변경 check
+            is SignupIntent.OnisPasswordVisible1Changed -> {
+                val current = _state.value.isPasswordVisible_1
+                _state.value = _state.value.copy(isPasswordVisible_1 = !current)
+
+            }
+            is SignupIntent.OnisPasswordVisible2Changed -> {
+                val current = _state.value.isPasswordVisible_2
+                _state.value = _state.value.copy(isPasswordVisible_2 = !current)
+
+            }
+
+            is SignupIntent.OnExpandedChanged -> {
+                val current = _state.value.expanded
+                _state.value = _state.value.copy(expanded = !current)
+            }
 
         }
     }
