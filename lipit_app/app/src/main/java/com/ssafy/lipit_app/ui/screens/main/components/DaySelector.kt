4116a22630aecfb8d50a79c3dc142710db161e84
gaga9353 <gaga9353@naver.com>
2025-04-05 00:35:03 +0900

    refactor: State에서 intState로 변경
    Former-commit-id: 467c7948149dd403e4eac930b389b959bf82d8bc

--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/main/components/DaySelector.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/main/components/DaySelector.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -39,7 +40,7 @@ fun DaySelector(
 
     // 현재 선택된 요일의 인덱스
     var currentIndex by remember {
-        mutableStateOf(days.indexOf(selectedDay).coerceIn(0, days.size - 1))
+        mutableIntStateOf(days.indexOf(selectedDay).coerceIn(0, days.size - 1))
     }
 
     Row(
