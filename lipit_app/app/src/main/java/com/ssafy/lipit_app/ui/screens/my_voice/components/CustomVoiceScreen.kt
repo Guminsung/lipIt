06f817feb8dfff996de49f0a406c50a0d378804a
gaga9353 <gaga9353@naver.com>
2025-04-06 01:11:40 +0900

    style: 커스텀 음성 선택 디자인 변경 S12P21D102-365
    Former-commit-id: 6ae10d7a607ed7f588ba7a9984d0353396134f3a

--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/my_voice/components/CustomVoiceScreen.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/my_voice/components/CustomVoiceScreen.kt
@@ -1,8 +1,13 @@
 package com.ssafy.lipit_app.ui.screens.my_voice.components
 
+import android.util.Log
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -31,13 +36,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.test.isSelected
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -56,6 +65,7 @@ import com.ssafy.lipit_app.data.model.response_dto.myvoice.CustomResponse
 @Composable
 fun CustomVoiceScreen(
     customVoices: List<CustomResponse> = emptyList(),
+    selectedVoiceName: String,
     onVoiceChange: (Long) -> Unit
 ) {
 
@@ -90,22 +100,20 @@ fun CustomVoiceScreen(
             ) {
                 // 커스텀 음성 목록 표시
                 items(customVoices) { voice ->
-
+                    Log.d("TAG", "CustomVoiceScreen: ${selectedVoiceName} ${voice.voiceName}")
                     CustomColumn(
                         voices = voice,
                         imageUrl = voice.customImageUrl,
                         voiceName = voice.voiceName,
+                        isSelected = selectedVoiceName == voice.voiceName,
                         onVoiceChange = { onVoiceChange(voice.voiceId) }
                     )
 
                     Spacer(modifier = Modifier.height(15.dp))
                 }
             }
-
         }
-
     }
-
 }
 
 @Composable
@@ -113,19 +121,40 @@ fun CustomColumn(
     voices: CustomResponse,
     imageUrl: String,
     voiceName: String,
-    onVoiceChange: (Long) -> Unit
+    onVoiceChange: (Long) -> Unit,
+    isSelected: Boolean = false
 ) {
 
     var isPlaying by remember { mutableStateOf(false) }
+    val rememberedSelected = rememberUpdatedState(isSelected).value
 
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(
-                Color.White,
-                shape = RoundedCornerShape(20.dp)
+            .graphicsLayer {
+                clip = false // 그림자 잘리지 않게 설정!
+            }
+            .shadow(
+                elevation = if (rememberedSelected) 8.dp else 2.dp,
+                shape = RoundedCornerShape(16.dp),
+                ambientColor = if (rememberedSelected) Color(0xFFA37BBD) else Color(0xFFCCCCCC),
+                spotColor = if (rememberedSelected) Color(0xFFD372FF) else Color(0xFFCCCCCC)
             )
-            .padding(horizontal = 16.dp, vertical = 12.dp),
+            .clip(RoundedCornerShape(16.dp))
+            .background(
+                color = if (rememberedSelected) Color(0xFFF9F0FF) else Color.White,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .border(
+                width = if (rememberedSelected) 2.dp else 0.dp,
+                color = Color(0xFFD372FF),
+                shape = RoundedCornerShape(16.dp)
+            )
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null // 클릭 효과(리플) 제거하여 깜빡임 최소화
+            ) { onVoiceChange(voices.voiceId) }
+            .padding(horizontal = 18.dp, vertical = 14.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
@@ -165,25 +194,6 @@ fun CustomColumn(
                 fontSize = 15.sp
             )
         }
-
-        //            Button(
-//                onClick = { onVoiceChange(voiceId) },
-//                colors = ButtonDefaults.buttonColors(Color.Transparent),
-//                border = BorderStroke(
-//                    width = 1.dp,
-//                    color = Color(0xffA37BBD)
-//                ),
-//                shape = RoundedCornerShape(20.dp),
-//                elevation = ButtonDefaults.elevation(0.dp),
-//                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-//            ) {
-//                Text(
-//                    "선택", fontWeight = FontWeight.Bold,
-//                    fontSize = 12.sp,
-//                    color = Color(0xffA37BBD)
-//                )
-//            }
-
 
         Icon(
             painter = painterResource(
