aa6109f80e4d42f1be3f122668aaeae157b974ec
서정후 <jeonghu1010@naver.com>
2025-04-08 13:56:01 +0900

    design: Report 리스트 디자인 보완

--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/report/components/ReportFront.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/report/components/ReportFront.kt
@@ -38,59 +38,48 @@ import com.ssafy.lipit_app.util.CommonUtils.formatSeconds
 // 카드 앞면
 @Composable
 fun ReportFront(
-    report: ReportListResponse,
-    onReportItemClick: (Long) -> Unit,
-    onMeasuredHeight: (Int) -> Unit
+    report: ReportListResponse, onReportItemClick: (Long) -> Unit, onMeasuredHeight: (Int) -> Unit
 ) {
 
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .onGloballyPositioned { layoutCoordinates ->
-                onMeasuredHeight(layoutCoordinates.size.height)
-            }
-            .paint(
-                painter = painterResource(id = R.drawable.report_background),
-                contentScale = ContentScale.FillBounds
-            )
-            .padding(horizontal = 30.dp, vertical = 40.dp)
-    ) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .onGloballyPositioned { layoutCoordinates ->
+            onMeasuredHeight(layoutCoordinates.size.height)
+        }
+        .paint(
+            painter = painterResource(id = R.drawable.report_background),
+            contentScale = ContentScale.FillBounds
+        )
+        .padding(horizontal = 15.dp, vertical = 40.dp)) {
 
         Column {
             Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.Bottom
             ) {
                 Text(
-                    text = formatDate(report.createdAt),
-                    color = Color.White,
-                    fontSize = 22.sp,
-                    fontWeight = FontWeight.Bold
+                    text = formatDate(report.createdAt), style = TextStyle(
+                        fontSize = 20.sp,
+                        lineHeight = 30.sp,
+                        fontWeight = FontWeight(700),
+                        color = Color(0xFFFFFFFF),
+                    ), modifier = Modifier.padding(start = 15.dp)
                 )
 
                 Box(
                     modifier = Modifier
-//                        .border(
-//                            1.dp,
-//                            color = Color.White.copy(0.7f),
-//                            shape = RoundedCornerShape(20.dp)
-//                        )
-//                        .clip(RoundedCornerShape(20.dp))
-                        //.background(Color.White.copy(0.1f), shape = RoundedCornerShape(20.dp))
-                        .clickable {
-                            onReportItemClick(report.reportId)
-                        }
+                        .fillMaxWidth()
                         .padding(horizontal = 8.dp),
                     contentAlignment = Alignment.TopEnd
                 ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
+                    Row(verticalAlignment = Alignment.CenterVertically,
                         modifier = Modifier
-                            .offset(x = 15.dp, y = -8.dp) //  시간남으면 전체 코드 리팩토링해서 offset 제거
-                    ) {
+                            .clickable {
+                                onReportItemClick(report.reportId)
+                            }
+                            .padding(bottom = 5.dp)) {
 
                         Text(
                             "상세보기 ",
@@ -102,8 +91,7 @@ fun ReportFront(
                         Image(
                             painter = painterResource(id = R.drawable.btn_report_detail),
                             contentDescription = null,
-                            modifier = Modifier
-                                .size(14.dp)
+                            modifier = Modifier.size(14.dp)
                         )
                     }
                 }
@@ -112,93 +100,98 @@ fun ReportFront(
 
             Text(
                 "착신 통화 ${formatSeconds(report.callDuration)}",
-                color = Color.White.copy(0.8f),
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Light
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    lineHeight = 30.sp,
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFFFFFFFF),
+                ),
+                modifier = Modifier.padding(horizontal = 15.dp)
             )
         }
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color(0xffFDF8FF), shape = RoundedCornerShape(18.dp))
-                .padding(horizontal = 32.dp, vertical = 16.dp),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
+        Box(
+            modifier = Modifier.padding(horizontal = 15.dp)
         ) {
-            Column(
+            Row(
                 modifier = Modifier
-                    .weight(1f)
-                    .offset(x = -10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-
+                    .fillMaxWidth()
+                    .background(Color(0xffFDF8FF), shape = RoundedCornerShape(18.dp))
+                    .padding(horizontal = 42.dp, vertical = 16.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(
-                    text = "${report.wordCount}개",
-                    style = TextStyle(
-                        fontSize = 15.sp,
-                        lineHeight = 15.sp,
-                        fontWeight = FontWeight(400),
-                        color = Color(0xFF000000),
-                        textAlign = TextAlign.Center
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .offset(x = -12.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+                    Text(
+                        text = "${report.wordCount}개", style = TextStyle(
+                            fontSize = 15.sp,
+                            lineHeight = 15.sp,
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF000000),
+                            textAlign = TextAlign.Center
+                        )
                     )
+
+                    Text(
+                        text = "말한 단어 수", style = TextStyle(
+                            fontSize = 12.sp,
+                            lineHeight = 15.sp,
+                            fontWeight = FontWeight(700),
+                            color = Color(0xFF000000),
+                            textAlign = TextAlign.Center,
+                        )
+                    )
+                }
+
+                VerticalDivider(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .width(2.dp), color = Color(0xffD3D3D3)
                 )
 
-                Text(
-                    text = "말한 단어 수",
-                    style = TextStyle(
-                        fontSize = 12.sp,
-                        lineHeight = 15.sp,
-                        fontWeight = FontWeight(700),
-                        color = Color(0xFF000000),
-                        textAlign = TextAlign.Center,
-                    )
-                )
-            }
 
-            VerticalDivider(
-                modifier = Modifier
-                    .height(40.dp)
-                    .width(2.dp),
-                color = Color(0xffD3D3D3)
-            )
-
-
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .offset(x = 10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "${report.sentenceCount}개",
-                    style = TextStyle(
-                        fontSize = 15.sp,
-                        lineHeight = 15.sp,
-                        fontWeight = FontWeight(400),
-                        color = Color(0xFF000000),
-                        textAlign = TextAlign.Center
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .offset(x = 12.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "${report.sentenceCount}개", style = TextStyle(
+                            fontSize = 15.sp,
+                            lineHeight = 15.sp,
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF000000),
+                            textAlign = TextAlign.Center
+                        )
                     )
-                )
-                Text(
-                    text = "말한 문장 수",
-                    style = TextStyle(
-                        fontSize = 12.sp,
-                        lineHeight = 15.sp,
-                        fontWeight = FontWeight(700),
-                        color = Color(0xFF000000),
-                        textAlign = TextAlign.Center,
+                    Text(
+                        text = "말한 문장 수", style = TextStyle(
+                            fontSize = 12.sp,
+                            lineHeight = 15.sp,
+                            fontWeight = FontWeight(700),
+                            color = Color(0xFF000000),
+                            textAlign = TextAlign.Center,
+                        )
                     )
-                )
+                }
             }
         }
 
         Spacer(modifier = Modifier.height(20.dp))
 
         // 요약 영역
-        Column {
+        Column(
+            modifier = Modifier.padding(horizontal = 15.dp)
+        ) {
             Text(
                 "대화 내용 요약",
                 style = TextStyle(
@@ -209,11 +202,11 @@ fun ReportFront(
                 )
             )
 
+
             Spacer(modifier = Modifier.height(7.dp))
 
             Text(
-                text = report.communicationSummary,
-                style = TextStyle(
+                text = report.communicationSummary, style = TextStyle(
                     fontSize = 14.sp,
                     lineHeight = 20.sp,
                     fontWeight = FontWeight(400),
@@ -224,10 +217,11 @@ fun ReportFront(
 
         Spacer(modifier = Modifier.height(15.dp))
 
-        Column {
+        Column(
+            modifier = Modifier.padding(horizontal = 15.dp)
+        ) {
             Text(
-                "AI 피드백 요약",
-                style = TextStyle(
+                "AI 피드백 요약", style = TextStyle(
                     fontSize = 18.sp,
                     lineHeight = 30.sp,
                     fontWeight = FontWeight(700),
@@ -238,13 +232,13 @@ fun ReportFront(
             Spacer(modifier = Modifier.height(7.dp))
 
             Text(
-                text = report.feedbackSummary,
-                style = TextStyle(
+                text = report.feedbackSummary, style = TextStyle(
                     fontSize = 14.sp,
                     lineHeight = 20.sp,
                     fontWeight = FontWeight(400),
                     color = Color(0xFFFFFFFF),
                 )
+
             )
         }
     }
