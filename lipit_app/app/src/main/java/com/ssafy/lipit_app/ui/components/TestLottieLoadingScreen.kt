115a2a8603f8da030875484bd3edc016eeac5082
gaga9353 <gaga9353@naver.com>
2025-04-09 04:55:52 +0900

    design: 리포트 생성 로딩 페이지 오버레이 추가

--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/components/TestLottieLoadingScreen.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/components/TestLottieLoadingScreen.kt
@@ -1,15 +1,20 @@
 package com.ssafy.lipit_app.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,31 +29,40 @@ import com.ssafy.lipit_app.R
 
 @Composable
 fun TestLottieLoadingScreen(text: String = "로딩 중...") {
+
     val composition by rememberLottieComposition(
         LottieCompositionSpec.RawRes(R.raw.loader)
     )
 
     Dialog(onDismissRequest = {}) {
 
-        Column(
+        Box(
             modifier = Modifier
-                .padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .clip(RoundedCornerShape(20.dp))
+                .background(Color.Black.copy(alpha = 0.6f), shape = RoundedCornerShape(20.dp)),
+            contentAlignment = Alignment.Center
         ) {
-            LottieAnimation(
-                composition = composition,
-                iterations = LottieConstants.IterateForever,
-                modifier = Modifier.size(120.dp)
-            )
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Column(
+                modifier = Modifier
+                    .padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                LottieAnimation(
+                    composition = composition,
+                    iterations = LottieConstants.IterateForever,
+                    modifier = Modifier.size(120.dp)
+                )
 
-            Text(
-                text = text,
-                color = Color(0xFFFAFAFA),
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-            )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = text,
+                    color = Color(0xFFFAFAFA),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
         }
     }
 }
@@ -61,22 +75,26 @@ fun PreviewTestLottieContentOnly() {
     )
 
     Column(
-        modifier = Modifier.padding(24.dp),
+        modifier = Modifier
+            .background(Color.White)
+            .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        LottieAnimation(
-            composition = composition,
-            iterations = LottieConstants.IterateForever,
-            modifier = Modifier.size(120.dp)
-        )
+//        LottieAnimation(
+//            composition = composition,
+//            iterations = LottieConstants.IterateForever,
+//            modifier = Modifier.size(120.dp)
+//        )
+//
+//        Spacer(modifier = Modifier.height(16.dp))
+//
+//        Text(
+//            text = "리포트 생성 중...",
+//            color = Color(0xFFFAFAFA),
+//            fontSize = 20.sp,
+//            fontWeight = FontWeight.Bold,
+//        )
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = "리포트 생성 중...",
-            color = Color(0xFFFAFAFA),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-        )
+        TestLottieLoadingScreen()
     }
 }
