74209432b6c18be4cf7e80c52f5369ae13bbcc4d
서정후 <jeonghu1010@naver.com>
2025-04-09 07:53:07 +0900

    design: My Voices UI 보완
    Former-commit-id: 34a380dc4437b4a4a125421e1e61406cc1e05736

--- lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/my_voice/MyVoiceScreen.kt
+++ lipit_app/app/src/main/java/com/ssafy/lipit_app/ui/screens/my_voice/MyVoiceScreen.kt
@@ -1,9 +1,9 @@
 package com.ssafy.lipit_app.ui.screens.my_voice
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
